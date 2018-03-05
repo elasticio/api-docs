642d6305b54143a3a6b9cbab3d157a4a1e2c8c1c
yarikos <yarik.ponomarenko@gmail.com>
2018-03-05 15:44:29 +0200

    update to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t api-docs .
-CURRENT_DIR=$(pwd)
-docker run -it -v $CURRENT_DIR:/api-docs api-docs:latest  bundle exec middleman build
+docker run --name api-docs-build --rm -it -v `pwd`:/api-docs api-docs:latest  bundle exec middleman build
